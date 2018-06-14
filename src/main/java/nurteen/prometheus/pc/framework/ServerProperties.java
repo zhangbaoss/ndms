fbe9f7f8974e263fa0264e362ed2579c6d285537
SoulW17 <weizhichao1717@163.com>
2018-06-14 12:34:15 +0800

    serverProperties

--- src/main/java/nurteen/prometheus/pc/framework/ServerProperties.java
+++ src/main/java/nurteen/prometheus/pc/framework/ServerProperties.java
@@ -1,5 +1,8 @@
 package nurteen.prometheus.pc.framework;
 
+import nurteen.prometheus.pc.framework.entities.DeviceInfo;
+import nurteen.prometheus.pc.framework.entities.DeviceType;
+
 public class ServerProperties {
     static String ndid;
 
@@ -7,13 +10,26 @@ public class ServerProperties {
         if (ndid != null) {
             return ndid;
         }
+        
         synchronized (ServerProperties.class) {
-            try {
-                ndid = ObjectFactory.storageAware.genNdid(Constants.DEVICE_TYPE_PC_CENTER);
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
+        	if (ndid != null) {
+	            try {
+	            	DeviceInfo deviceInfo = ObjectFactory.storageAware.fromHid("");
+	            	if (deviceInfo != null) {
+	            		ndid = deviceInfo.getNdid();
+	            	}
+	            	else {
+	            		// ndid = ObjectFactory.storageAware.genNdid(DeviceType.Center_Pc);
+	            		deviceInfo = new DeviceInfo(DeviceType.Center_Pc, 1, "");
+	            		ObjectFactory.storageAware.insertNew(deviceInfo);
+	            		ndid = deviceInfo.getNdid();
+	            	}
+	                
+	            }
+	            catch (Exception e) {
+	                e.printStackTrace();
+	            }
+        	}
         }
         return ndid;
     }
