25974aba70f685030a3b289999a43b45991ca27f
zhangbao <zhangbao@nurteen.com>
2018-06-19 10:25:32 +0800

    添加redis配置信息和序列化依赖

--- src/main/java/nurteen/prometheus/pc/framework/ServerConfigProperties.java
+++ src/main/java/nurteen/prometheus/pc/framework/ServerConfigProperties.java
@@ -1,10 +1,18 @@
 package nurteen.prometheus.pc.framework;
 
 public class ServerConfigProperties extends ServerProperties {
-    String idGenPrefix;
+	String idGenPrefix;
     String serverAddress;
     Integer wsRequestTimeout;
     Integer wsForwardTimeout;
+    String redisHost;
+    String redisPassword;
+    Integer redisPort;
+    Integer redisTimeout;
+    Integer redisPoolMaxTotal;
+    Integer redisPoolMaxIdle;
+    Integer redisPoolMaxWaitMillis;
+    Boolean redisPoolTestOnBorrow;
 
     public ServerConfigProperties() {
         this.idGenPrefix = "11";
@@ -38,4 +46,68 @@ public class ServerConfigProperties extends ServerProperties {
     public void setWsForwardTimeout(Integer wsForwardTimeout) {
         this.wsForwardTimeout = wsForwardTimeout;
     }
+
+	public String getRedisHost() {
+		return redisHost;
+	}
+
+	public void setRedisHost(String redisHost) {
+		this.redisHost = redisHost;
+	}
+
+	public String getRedisPassword() {
+		return redisPassword;
+	}
+
+	public void setRedisPassword(String redisPassword) {
+		this.redisPassword = redisPassword;
+	}
+
+	public Integer getRedisPort() {
+		return redisPort;
+	}
+
+	public void setRedisPort(Integer redisPort) {
+		this.redisPort = redisPort;
+	}
+
+	public Integer getRedisTimeout() {
+		return redisTimeout;
+	}
+
+	public void setRedisTimeout(Integer redisTimeout) {
+		this.redisTimeout = redisTimeout;
+	}
+
+	public Integer getRedisPoolMaxTotal() {
+		return redisPoolMaxTotal;
+	}
+
+	public void setRedisPoolMaxTotal(Integer redisPoolMaxTotal) {
+		this.redisPoolMaxTotal = redisPoolMaxTotal;
+	}
+
+	public Integer getRedisPoolMaxIdle() {
+		return redisPoolMaxIdle;
+	}
+
+	public void setRedisPoolMaxIdle(Integer redisPoolMaxIdle) {
+		this.redisPoolMaxIdle = redisPoolMaxIdle;
+	}
+
+	public Integer getRedisPoolMaxWaitMillis() {
+		return redisPoolMaxWaitMillis;
+	}
+
+	public void setRedisPoolMaxWaitMillis(Integer redisPoolMaxWaitMillis) {
+		this.redisPoolMaxWaitMillis = redisPoolMaxWaitMillis;
+	}
+
+	public Boolean getRedisPoolTestOnBorrow() {
+		return redisPoolTestOnBorrow;
+	}
+
+	public void setRedisPoolTestOnBorrow(Boolean redisPoolTestOnBorrow) {
+		this.redisPoolTestOnBorrow = redisPoolTestOnBorrow;
+	}
 }
